3dfe723fa7888cef10ec21439bf4586562d79476
anthony.buchholz <anthony.buchholz@gmail.com>
2019-12-27 14:57:55 -0600

    Convert local mongoDB to clever cloud.

--- src/mongo/mongo-init.ts
+++ src/mongo/mongo-init.ts
@@ -1,8 +1,14 @@
 import mongoose from "mongoose";
-mongoose.connect("mongodb://localhost/credo", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose.connect(
+  `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@bwlk74sflmfqzlc-mongodb.services.clever-cloud.com:27017/bwlk74sflmfqzlc`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
 let db = mongoose.connection;
 db.once("open", () => {});
 
